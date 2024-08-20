f4a22cdf46beae4a3c81398a65085b164e2f4a31
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-08-20 18:41:43 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './comonents/Navbar';
+import ProductsList from './comonents/ProductsList';
+import Slider from './comonents/Slider';
+import About from './comonents/About';
+import Details from './comonents/Details';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<><Slider /><ProductsList /></>}></Route>
+          <Route path='about' element={<About/>}></Route>
+          <Route path='product/:productId' element={<Details/>}></Route>
+        </Routes>
+      
+      
     </div>
   );
 }
